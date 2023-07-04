ff1f37b5eaba2939235177695cc94edcd1ada44d
Johannes Köster <johannes.koester@uni-due.de>
2023-07-04 12:02:04 +0200

    adapt to changes in requirements

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,4 +3,7 @@
 # Make sure bindgen passes on our compiler flags.
 export BINDGEN_EXTRA_CLANG_ARGS="${CPPFLAGS} ${CFLAGS} ${LDFLAGS}"
 
+# deploy npx for installation of JS dependencies
+npm install --global npx webpack-cli
+
 cargo install --no-track --locked --verbose --root "${PREFIX}" --path .
