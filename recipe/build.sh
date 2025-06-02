6d4648887fa707df2735c301244e9af1277d81f9
Uwe L. Korn <uwe.korn@quantco.com>
2025-06-02 21:53:08 +0200

    Try without patching

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,9 +4,6 @@ set -exuo pipefail
 
 export RUSTFLAGS="$CARGO_BUILD_RUSTFLAGS -L${PREFIX}/lib"
 
-if [[ "${build_platform}" != "${target_platform}" ]]; then
-  cargo patch
-fi
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
 # Make sure bindgen passes on our compiler flags.
